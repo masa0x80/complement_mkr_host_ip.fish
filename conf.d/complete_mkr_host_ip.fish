d0b499ace933bde422f90142f5fcb1fb7a134e08
KIMURA Masayuki <masa0x80@gmail.com>
2017-08-23 23:47:01 +0900

    Fix indent

--- conf.d/complete_mkr_host_ip.fish
+++ conf.d/complete_mkr_host_ip.fish
@@ -1,5 +1,7 @@
 function __register_complete_mkr_host_ip_keybind --on-event fish_prompt
-  not set -q COMPLETE_MKR_HOST_IP_KEYBIND; and set -l COMPLETE_MKR_HOST_IP_KEYBIND '\co\co'
-  eval bind $COMPLETE_MKR_HOST_IP_KEYBIND __complete_mkr_host_ip
-  functions -e __register_complete_mkr_host_ip_keybind
+    if not set -q COMPLETE_MKR_HOST_IP_KEYBIND
+        set COMPLETE_MKR_HOST_IP_KEYBIND '\co\co'
+    end
+    eval bind $COMPLETE_MKR_HOST_IP_KEYBIND __complete_mkr_host_ip
+    functions -e __register_complete_mkr_host_ip_keybind
 end
