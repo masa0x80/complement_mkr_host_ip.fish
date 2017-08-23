5b1911015b1bfdfc6c72cd5c6e65390e7caff923
KIMURA Masayuki <masa0x80@gmail.com>
2017-08-24 00:01:08 +0900

    Enable multiple selections

--- functions/__complete_mkr_host_ip.fish
+++ functions/__complete_mkr_host_ip.fish
@@ -1,15 +1,17 @@
 function __complete_mkr_host_ip
-  set -l selected_line
-  if string match -q -- "$ANGLER" 'fzf'
-    mkr_hosts_tsv | fzf --nth=2.. --delimiter='\t' | read selected_line
-  else
-    mkr_hosts_tsv | angler | read selected_line
-  end
+    if string match -q -- "$ANGLER" 'fzf'
+        mkr_hosts_tsv | fzf --nth=2.. --delimiter='\t' | tr ';' ' ' | tr '\n' ';' | read selected_lines
+    else
+        mkr_hosts_tsv | angler | tr ';' ' ' | tr '\n' ';' | read selected_lines
+    end
 
-  if test -n "$selected_line"
-    set -l ip   (echo $selected_line | cut -f 1)
-    set -l host (echo $selected_line | cut -f 2)
-    commandline -a "$ip # $host"
-  end
-  commandline -f repaint
+    if test -n "$selected_lines"
+        for line in (string split ';' $selected_lines)
+            echo $line | cut -f 1,2 | read ip host
+            set ips $ips $ip
+            set hosts $hosts $host
+        end
+        commandline -a -- (string join -- '# ' (string join -- ' ' $ips) (string join -- ' ' $hosts))
+    end
+    commandline -f repaint
 end
